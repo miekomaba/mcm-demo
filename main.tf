e4a6ce15594412e6a786a43ec35274b487f4d0b9
miekomaba <85153567+miekomaba@users.noreply.github.com>
2021-10-25 17:01:14 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
-resource "null_resource" "demo_hello" {
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "example-organization"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "example-workspace"
+    }
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
   provisioner "local-exec" {
     command = "echo Hello World!!"
   }
